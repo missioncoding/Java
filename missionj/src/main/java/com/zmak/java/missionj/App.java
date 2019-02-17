0e9751b035d854a8a6305794bafea7c68f2919c3
zee <er.zeelumak@gmail.com>
2019-02-17 17:23:51 +0530

    setting up the space

---
+++ missionj/src/main/java/com/zmak/java/missionj/App.java
@@ -0,0 +1,13 @@
+package com.zmak.java.missionj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
